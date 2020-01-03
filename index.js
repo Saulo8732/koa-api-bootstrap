4c4f6a27480fd5539e19cbb0aa0c49ee200eac8a
Saulo Santiago <saulo8732@hotmail.com>
2020-01-02 22:28:54 -0300

    Creating initial structure

---
+++ index.js
@@ -0,0 +1,13 @@
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+
+const files = require('./lib/files');
+
+clear();
+
+console.log(
+  chalk.yellow(
+    figlet.textSync('Koa-CLI', { horizontalLayout: 'full' })
+  )
+);
